ec2b9685cd7bbe594ddd5cc5992dd2b091251d6e
Jessica Santana <jeafsantana@hotmail.com>
2021-08-06 19:43:35 -0300

    Validação do CPF

--- meuPrimeiroProjeto/meuPrimeiroProjeto/ViewController.swift
+++ meuPrimeiroProjeto/meuPrimeiroProjeto/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cpfTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func isValidCPF() -> Bool {
+        if cpfTextField.text == "" {
+            return false
+        }
+        
+        if cpfTextField.text?.count != 11 {
+            return false
+        }
+        
+        let numericCPF = Int(cpfTextField.text!)
+        
+        if numericCPF == nil {
+            return false
+        }
+        
+        return true
+    }
+    
+    
+    @IBAction func okButtonAction(_ sender: Any) {
+        
+        if !isValidCPF() {
+            cpfTextField.layer.borderWidth = 1
+            cpfTextField.layer.borderColor = UIColor.red.cgColor
+        } else {
+            cpfTextField.layer.borderWidth = 1
+            cpfTextField.layer.borderColor = UIColor.clear.cgColor
+        }
+    }
 
 }
 
